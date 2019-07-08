9b7f1313b1010615f440f9e4909565f54c221428
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-07-08 22:47:10 +0300

    add transfer learning

---
+++ transfer_learning/data_prep.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+import helper
+
+
+def get_data(data_dir=str(Path.home()) + '/data/cat_and_dog'):
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                           transforms.RandomResizedCrop(224),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.485, 0.456, 0.406],
+                                                                [0.229, 0.224, 0.225])])
+
+    test_transforms = transforms.Compose([transforms.Resize(255),
+                                          transforms.CenterCrop(224),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize([0.485, 0.456, 0.406],
+                                                               [0.229, 0.224, 0.225])])
+
+    # Pass transforms in here, then run the next cell to see how the transforms look
+    train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
+    test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
+
+    trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=64)
+
+    return trainloader, testloader
+
+
+if __name__ == '__main__':
+    data_dir = str(Path.home()) + '/data/cat_and_dog'
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                           transforms.RandomResizedCrop(224),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor()])
+
+    test_transforms = transforms.Compose([transforms.Resize(255),
+                                          transforms.CenterCrop(224),
+                                          transforms.ToTensor()])
+
+    # Pass transforms in here, then run the next cell to see how the transforms look
+    train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
+    test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
+
+    trainloader = torch.utils.data.DataLoader(train_data, batch_size=32)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=32)
+    data_iter = iter(testloader)
+
+    images, labels = next(data_iter)
+    fig, axes = plt.subplots(figsize=(10, 4), ncols=4)
+    for ii in range(4):
+        ax = axes[ii]
+        helper.imshow(images[ii], ax=ax, normalize=False)
+    plt.show()
---
+++ transfer_learning/train.py
@@ -0,0 +1,73 @@
+import torch
+from torch import nn
+from torchvision import models
+from torch import optim
+
+from data_prep import *
+
+
+def train_model(model, trainloader, testloader, criterion, optimizer,
+                epochs=1, steps=0, running_loss=0, print_every=5):
+    for epoch in range(epochs):
+        for inputs, labels in trainloader:
+            steps += 1
+
+            optimizer.zero_grad()
+
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in testloader:
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+
+                        test_loss += batch_loss.item()
+
+                        # Calculate accuracy
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+                print(f"Epoch {epoch + 1}/{epochs}.. "
+                      f"Train loss: {running_loss / print_every:.3f}.. "
+                      f"Test loss: {test_loss / len(testloader):.3f}.. "
+                      f"Test accuracy: {accuracy / len(testloader):.3f}")
+                running_loss = 0
+                model.train()
+
+
+def get_model():
+    model = models.densenet121(pretrained=True)
+
+    # Freeze parameters so we don't backprop through them
+    for param in model.parameters():
+        param.requires_grad = False
+
+    model.classifier = nn.Sequential(nn.Linear(1024, 256),
+                                     nn.ReLU(),
+                                     nn.Dropout(0.2),
+                                     nn.Linear(256, 2),
+                                     nn.LogSoftmax(dim=1))
+    return model
+
+
+if __name__ == '__main__':
+    model = get_model()
+
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
+
+    trainloader, testloader = get_data()
+
+    train_model(model, trainloader, testloader, criterion, optimizer)
+
