8929b4773dbb21e19a567f04c7ccc6e643deb5e7
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-07-10 20:57:23 +0300

    changed epochs to 4

--- sentiment_analysis/train.py
+++ sentiment_analysis/train.py
@@ -10,7 +10,7 @@ import time
 
 args_train = Namespace(
     lr=0.001,
-    epochs=1,
+    epochs=4,
     print_every=100,
     clip=5
 )
@@ -74,11 +74,57 @@ def train(net, train_loader, valid_loader, optimizer, criterion,
     torch.save(net.state_dict(), filename)
 
 
+def test_model(net, test_loader, batch_size=args_model.batch_size):
+    # Get test data loss and accuracy
+
+    test_losses = []  # track loss
+    num_correct = 0
+
+    # init hidden state
+    h = net.init_hidden(batch_size)
+
+    net.eval()
+    # iterate over test data
+    for inputs, labels in test_loader:
+
+        # Creating new variables for the hidden state, otherwise
+        # we'd backprop through the entire training history
+        h = tuple([each.data for each in h])
+
+        # get predicted outputs
+        output, h = net(inputs, h)
+
+        # calculate loss
+        test_loss = criterion(output.squeeze(), labels.float())
+        test_losses.append(test_loss.item())
+
+        # convert output probabilities to predicted class (0 or 1)
+        pred = torch.round(output.squeeze())  # rounds to the nearest integer
+
+        # compare predictions to true label
+        correct_tensor = pred.eq(labels.float().view_as(pred))
+        correct = np.squeeze(correct_tensor.numpy())
+        num_correct += np.sum(correct)
+
+    # -- stats! -- ##
+    # avg test loss
+    print("Test loss: {:.3f}".format(np.mean(test_losses)))
+
+    # accuracy over all test data
+    test_acc = num_correct / len(test_loader.dataset)
+    print("Test accuracy: {:.3f}".format(test_acc))
+
+
 if __name__ == '__main__':
     train_loader, valid_loader, test_loader = get_batch()
 
     net = SentimentRNN(args_model.vocab_size, args_model.output_size,
                        args_model.embedding_dim, args_model.hidden_dim, args_model.n_layers)
+
     criterion = nn.BCELoss()
     optimizer = torch.optim.Adam(net.parameters(), lr=args_train.lr)
     train(net, train_loader, valid_loader, optimizer, criterion)
+
+    # filename = 'models/model_1_1562780581.pth'
+    # net.load_state_dict(torch.load(filename))
+    # test_model(net, test_loader)
